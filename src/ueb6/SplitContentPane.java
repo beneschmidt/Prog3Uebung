ef566cf79b2bf06c571ca4120b1792b670a902cc
Benne <benediktschmidt89@gmail.com>
2013-04-28 20:39:48 +0200

    Setzen der Liste ein wenig erweitert

--- src/ueb6/SplitContentPane.java
+++ src/ueb6/SplitContentPane.java
@@ -14,17 +14,33 @@ public class SplitContentPane extends JSplitPane {
 	private JTabbedPane tabbedPane;
 
 	public SplitContentPane() {
-		list = new JList<>();
-		DefaultListModel<File> model = new DefaultListModel<>();
-		File file = new File(System.getProperty("user.dir"));
-		for (File nextFile : file.listFiles()) {
-			model.addElement(nextFile);
-		}
-		list.setModel(model);
+		initList();
 		tabbedPane = new JTabbedPane();
 		setLayout(new BorderLayout());
 		this.add(list, BorderLayout.WEST);
 		this.add(tabbedPane, BorderLayout.EAST);
+	}
+
+	/**
+	 * Initialisiert die Liste, in dem der aktuelle Ordner ausgelesen wird und alle vorhandenen Dateien in die Liste gefuegt werden.
+	 */
+	private void initList() {
+		list = new JList<>();
+		File rootFile = new File(System.getProperty("user.dir"));
+		loadFilesInFolder(rootFile);
+	}
+
+	/**
+	 * Liest alle Dateien aus einem uebergebene Ordner aus und wirft sie in die Liste
+	 * 
+	 * @param rootFile
+	 */
+	public void loadFilesInFolder(File rootFile) {
+		DefaultListModel<File> model = new DefaultListModel<>();
+		for (File nextFile : rootFile.listFiles()) {
+			model.addElement(nextFile);
+		}
+		list.setModel(model);
 	}
 
 }
